633146134a4942efab636b2bfd5d471a66aad9b5
Hayoun Lee <sidhlee@gmail.com>
2021-01-06 14:11:44 -0500

    27. Create data schema

--- src/craiglist-web-scrapper/index.ts
+++ src/craiglist-web-scrapper/index.ts
@@ -1,5 +1,19 @@
 import puppeteer from 'puppeteer'
 
+const scrapingResults = [
+  {
+    title: 'Sales Representative (Seafood)',
+    datePosted: new Date('2021-01-06 12:00:00'),
+    neighborhood: 'Toronto, ON (Remote work to start)',
+    url:
+      'https://toronto.craigslist.org/tor/sls/d/toronto-sales-representative-seafood/7257905978.html',
+    jobDescription: `An established global trading company is looking for an energetic sales representative in Toronto area.
+You are responsible for the domestic frozen seafood market. The ideal candidate would possess at least five years of sales experience in a similar setting.
+`,
+    compensation: '$50K – 100K (DOE)',
+  },
+]
+
 // Using puppeteer instead of request because Craiglist has been blocking scraping IP addresses
 export default async function main() {
   // initialize and open up browser (chromium)
