c9528085787a51125ace9658a8a13d607cccec1c
Hayoun Lee <sidhlee@gmail.com>
2021-01-08 15:18:51 -0500

    64. Implementparser.listings and get title test to pass

--- src/tdd-scraper/parser.ts
+++ src/tdd-scraper/parser.ts
@@ -1,1 +1,15 @@
-export const add = (a: number, b: number) => a + b
+import cheerio from 'cheerio'
+
+// export const add = (a: number, b: number) => a + b
+
+export const listings = (html: string) => {
+  const $ = cheerio.load(html)
+  const listings = $('.result-info')
+    .map((i, infoDiv) => {
+      const title = $(infoDiv).find('.result-title').text()
+      return { title }
+    })
+    .get()
+
+  return listings
+}
