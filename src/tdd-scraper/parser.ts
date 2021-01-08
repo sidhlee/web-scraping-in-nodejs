40ed22128a4b3723f06db744384b9460b3228a0f
Hayoun Lee <sidhlee@gmail.com>
2021-01-08 17:05:00 -0500

    68. Refactor parser.listings

--- src/tdd-scraper/parser.ts
+++ src/tdd-scraper/parser.ts
@@ -3,6 +3,16 @@ import { cleanUp } from '../craiglist-web-scrapper'
 
 // export const add = (a: number, b: number) => a + b
 
+const getDatePosted = ($: cheerio.Root, elem: cheerio.Element) => {
+  const dateString = $(elem).find('time').attr('datetime') as string
+  return new Date(dateString)
+}
+
+const getNeighborhood = ($: cheerio.Root, elem: cheerio.Element) => {
+  const rawNeighborhoodString = $(elem).find('.result-hood').text()
+  return cleanUp(rawNeighborhoodString)
+}
+
 export const listings = (html: Buffer) => {
   const $ = cheerio.load(html)
   const listings = $('.result-info')
@@ -10,10 +20,8 @@ export const listings = (html: Buffer) => {
       const $listingLink = $(infoDiv).find('.result-title')
       const title = $listingLink.text()
       const url = $listingLink.attr('href')
-      const datePosted = new Date(
-        $(infoDiv).find('time').attr('datetime') as string
-      )
-      const neighborhood = cleanUp($(infoDiv).find('.result-hood').text())
+      const datePosted = getDatePosted($, infoDiv)
+      const neighborhood = getNeighborhood($, infoDiv)
 
       return { title, url, datePosted, neighborhood }
     })
