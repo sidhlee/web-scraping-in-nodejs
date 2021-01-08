fa6b495039de9d915826d270063a5f433a8ef858
Hayoun Lee <sidhlee@gmail.com>
2021-01-08 15:05:02 -0500

    63. Write tests for the parser.listings(html)

--- src/tdd-scraper/__tests__/parser.test.ts
+++ src/tdd-scraper/__tests__/parser.test.ts
@@ -1,15 +1,49 @@
 import * as parser from '../parser'
 import path from 'path'
 import fs from 'fs'
+import { IListing } from 'craiglist-web-scrapper/models/listing'
 
-let html
+let html: string, listings: IListing[]
 
 beforeAll(() => {
   const filePath = path.join(__dirname, '../test.html')
   html = fs.readFileSync(filePath)
+  listings = parser.listings(html)
 })
 
-it('should give 4', () => {
-  const result = parser.add(2, 2)
-  expect(result).toBe(4)
+// const listings = [
+//   {
+//     title: 'Outsource Agent Required',
+//     url:
+//       'https://toronto.craigslist.org/tor/acc/d/east-york-outsource-agent-required/7258922103.html',
+//     datePosted: '2021-01-08 12:47',
+//     neighborhood: 'Toronto',
+//   },
+// ]
+
+// it('should give 4', () => {
+//   const result = parser.add(2, 2)
+//   expect(result).toBe(4)
+// })
+
+it('should give the correct listing object', () => {
+  expect(listings.length).toBe(120)
+})
+
+it('should get correct title', () => {
+  expect(listings[0].title).toBe('Outsource Agent Required')
+})
+
+it('should get correct url', () => {
+  expect(listings[0].url).toBe(
+    'https://toronto.craigslist.org/tor/acc/d/east-york-outsource-agent-required/7258922103.html'
+  )
+})
+
+it('should get neighborhood from listing', () => {
+  expect(listings[0].neighborhood).toBe('Toronto')
+})
+
+it('should get correct date from listing', () => {
+  expect(listings[0].datePosted).toBe(new Date('2021-01-08 12:47'))
 })
